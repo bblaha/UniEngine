410333ded26b970ca9409f0b8d5dacf564eda251
Alexander Kröll <hi@akroell.com>
2020-04-23 14:20:11 +0200

    fixed resource tech level 0 reducing production efficiency to 0...

--- includes/vars_resproduction.php
+++ includes/vars_resproduction.php
@@ -16,7 +16,7 @@ if(defined('INSIDE'))
                         ]);
 
                 return [
-                    'metal' => ((30 * $level * pow((1.1), $level)) * (0.1 * $productionFactor)) * ((0.3) * pow(($resourceTechs[0]), (0.7))),
+                    'metal' => ((30 * $level * pow((1.1), $level)) * (0.1 * $productionFactor)) * (1 + ((0.3) * pow(($resourceTechs[0]), (0.7)))),
                     'energy' => (-1 * ((10 * $level * pow((1.1), $level)) * (0.1 * $productionFactor)))
                 ];
             }
@@ -31,7 +31,7 @@ if(defined('INSIDE'))
                 ]);
 
                 return [
-                    'crystal' => ((20 * $level * pow((1.1), $level)) * (0.1 * $productionFactor)) * ((0.3) * pow(($resourceTechs[0]), (0.7))),
+                    'crystal' => ((30 * $level * pow((1.1), $level)) * (0.1 * $productionFactor)) * (1 + ((0.3) * pow(($resourceTechs[0]), (0.7)))),
                     'energy' => (-1 * ((10 * $level * pow((1.1), $level)) * (0.1 * $productionFactor)))
                 ];
             }
@@ -47,7 +47,7 @@ if(defined('INSIDE'))
                 ]);
 
                 return [
-                    'deuterium' => (((10 * $level * pow((1.1), $level)) * (-0.002 * $planetTemp + 1.28)) * (0.1 * $productionFactor)) * ((0.3) * pow(($resourceTechs[0]), (0.7))),
+                    'deuterium' => (((10 * $level * pow((1.1), $level)) * (-0.002 * $planetTemp + 1.28)) * (0.1 * $productionFactor)) * (1 + ((0.3) * pow(($resourceTechs[0]), (0.7)))),
                     'energy' => (-1 * ((20 * $level * pow((1.1), $level)) * (0.1 * $productionFactor)))
                 ];
             }
